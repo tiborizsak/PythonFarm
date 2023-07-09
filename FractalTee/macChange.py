fa8e206510c83dfa78216b71c571df8a762d92a6
Tibor Izsak <tibor.izsak89@gmail.com>
2023-07-09 22:44:37 +0200

    macChanger v2

--- FractalTee/macChange.py
+++ FractalTee/macChange.py
@@ -1,6 +1,12 @@
 import subprocess
 import optparse
 
+def getArguments():
+    pharser = optparse.OptionParser()
+    pharser.add_option("-i", "--iface", dest="iface", help="Interface on which mac change performed")
+    pharser.add_option("-m", "--mac", dest="mod_mac", help="New mac")
+    return pharser.parse_args()
+
 def changeMac(iface, mod_mac):
     print("[@] Changing MAC to: " + mod_mac + " on " + iface)
 
@@ -8,11 +14,6 @@ def changeMac(iface, mod_mac):
     subprocess.call(["ifconfig", iface, "hw", "ether", mod_mac])
     subprocess.call(["ifconfig", iface, "up"])
 
-pharser = optparse.OptionParser()
-
-pharser.add_option("-i", "--iface", dest="iface", help="Interface on which mac change performed")
-pharser.add_option("-m", "--mac", dest="mod_mac", help="New mac")
-
-(pharserOpt, argum) = pharser.parse_args()
+(pharserOpt, argum) = getArguments()
 
 changeMac(pharserOpt.iface, pharserOpt.mod_mac)
