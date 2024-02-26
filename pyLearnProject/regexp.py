73cf333c9194240d3cca041de2c4f581638c8e14
Tibor Izsak <46084458+tiborizsak@users.noreply.github.com>
2024-02-26 08:30:44 +0100

    Update regexp.py

--- pyLearnProject/regexp.py
+++ pyLearnProject/regexp.py
@@ -78,3 +78,12 @@ namesRegex.sub('CENSORED', 'Agent Alice gave the secret documents to Agent Bob.'
 agentNamesRegex = re.compile(r'Agent (\w)\w*')
 agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent 
 Eve knew Agent Bob was a double agent.')
+
+phoneRegex = re.compile(r'''(
+ (\d{3}|\(\d{3}\))? # area code
+ (\s|-|\.)? # separator
+ \d{3}      # first 3 digits
+ (\s|-|\.)  # separator
+ \d{4}      # last 4 digits
+(\s*(ext|x|ext.)\s*\d{2,5})? # extension
+ )''', re.VERBOSE)
