d0bd67baa49e1fc6b4481b406e5378bca1433ad5
Tibor Izsak <46084458+tiborizsak@users.noreply.github.com>
2024-02-26 08:28:22 +0100

    Update regexp.py

--- pyLearnProject/regexp.py
+++ pyLearnProject/regexp.py
@@ -74,3 +74,7 @@ robocop = re.compile(r'robocop', re.I) # ignore case
 
 namesRegex = re.compile(r'Agent \w+')
 namesRegex.sub('CENSORED', 'Agent Alice gave the secret documents to Agent Bob.')
+
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
+agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent 
+Eve knew Agent Bob was a double agent.')
