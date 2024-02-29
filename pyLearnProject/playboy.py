765cdb2d24a21eff624745a53b3cf25958f83bcb
Tibor Izsak <46084458+tiborizsak@users.noreply.github.com>
2024-02-29 19:15:46 +0100

    Update playboy.py

--- pyLearnProject/playboy.py
+++ pyLearnProject/playboy.py
@@ -12,3 +12,18 @@ while toss == guess:
     print('Yaay')
   else:
     print('Agaaaain!')
+
+def guessTheNumber(numberofrounds, finite)
+  if numberofrounds > 0:
+    for i in range(numberofrounds + 1):
+      toss = random.randint(0, 1)
+      guess = guessList.index(input('Gimme guess! : '))
+      if toss == guess:
+        print('yaaaaaay')
+      else:
+        print()
+
+def guessthenumma():
+  toss = random.randint(0, 1)
+  guess = guessList.index(input('Gimme output! : '))
+  return toss == guess
