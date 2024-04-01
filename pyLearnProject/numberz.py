63e9734aa2065e25b7cda4fb696860089aeed270
Tibor, Izsak <tibor.izsak89@gmail.com>
2024-04-02 00:34:33 +0200

    240401 // pylearning numbers

---
+++ pyLearnProject/numberz.py
@@ -0,0 +1,14 @@
+# Besides usual math operations: + - * / %
+# Exponents marked with two *
+
+3 ** 2
+4 ** 4
+
+# If float is included in operation the result will always default to float
+# Numbers coul be grouped with _ it will be considered as normal int
+
+ageOfUniverse = 14_000_000_000_000
+
+# There is no method in python for constants but as a best practice those variables must be written with capital letters
+
+MAX_CONNECTION = 500
